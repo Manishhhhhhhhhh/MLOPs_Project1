3a98c7aec3100d1ad1f7a608be45e500785cfb07
Manish <manish.chauhan.we@gmail.com>
2025-05-11 21:51:00 +0530

    Project Structure Ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Manish Chauhan",
+    author_email="manish.chauhan.we@gmail.com",
+    packages=find_packages()
+)
